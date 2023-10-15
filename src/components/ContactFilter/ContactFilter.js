0b7cd268dda4189c3434ddaef44f2cf06834e189
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-10-15 18:53:15 +0300

    fix number label

--- src/components/ContactFilter/ContactFilter.js
+++ src/components/ContactFilter/ContactFilter.js
@@ -1,26 +1,3 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setFilter } from 'redux/filterSlice';
-// import { selectFilter } from 'redux/selectors';
-// import { FilterInputBox, FilterText } from './ContactFilter.styled';
-
-// export const ContactFilter = () => {
-//   const filter = useSelector(selectFilter);
-//   const dispatch = useDispatch();
-//   return (
-//     <>
-//       <FilterText>Find contacts by name</FilterText>
-//       <FilterInputBox
-//         type="text"
-//         value={filter}
-//         onChange={evt => {
-//           dispatch(setFilter(evt.target.value));
-//         }}
-//       />
-//     </>
-//   );
-// };
-
-
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
 import { selectFilter } from 'redux/selectors';
